ca79299c715985823f3ae9fc767df0819a466d0a
WillVanBuskirk <47007982+WillVanBuskirk@users.noreply.github.com>
2020-12-16 16:09:08 -0800

    Add project files.

---
+++ GeoAggregationManager.cs
@@ -0,0 +1,21 @@
+﻿using log4net;
+using System.Diagnostics;
+using System;
+using System.Net;
+
+namespace GeoAggregator
+{
+    class GeoAggregationManager
+    {
+        public static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        enum DTMErrors
+        {
+            webRequestFailure = 16002,
+            nullExeption = 16001
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
